e6e21f7c458a0fcae07988f305fd44b4d3cab55e
EliseBriers <elise.briers@outlook.com>
2022-11-20 13:13:37 +0100

    Small tweaks

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -7,3 +7,15 @@ lvim.builtin.alpha.mode = "dashboard"
 lvim.builtin.terminal.active = true
 lvim.builtin.nvimtree.setup.view.side = "left"
 lvim.builtin.nvimtree.setup.renderer.icons.show.git = false
+
+local options = {
+    shiftwidth = 4, -- the number of spaces inserted for each indentation
+    tabstop = 4, -- insert 2 spaces for a tab
+    fileencoding = "utf-8", -- the encoding written to a file
+    scrolloff = 0,
+    sidescrolloff = 8,
+}
+
+for k, v in pairs(options) do
+    vim.opt[k] = v
+end
